c25d68e942745182974de5a4349216d0113b8c65
Vadym Markov <markov.vadym@gmail.com>
2016-01-04 13:10:30 +0100

    Improve init script

--- init.rb
+++ init.rb
@@ -11,29 +11,44 @@ end
 
 folder_path = __dir__
 
-default_pod_name = 'MyPod'
+default_package_name = 'MyPackage'
+default_bundle_domain = 'no.hyper'
 default_author_name = 'Hyper Interaktiv AS'
 default_author_email = 'ios@hyper.no'
 default_username = 'hyperoslo'
 
-pod_name = ARGV.shift || prompt('pod name', default_pod_name) || default_pod_name
-author_name = prompt('author', default_author_name) || default_author_name
-author_email = prompt('e-mail', default_author_email) || default_author_email
-username = prompt('username', default_username) || default_username
+package_name = ARGV.shift || prompt('Package name', default_package_name) || default_package_name
+bundle_domain = prompt('Bundle domain', default_bundle_domain) || default_bundle_domain
+author_name = prompt('Author', default_author_name) || default_author_name
+author_email = prompt('E-mail', default_author_email) || default_author_email
+username = prompt('Username', default_username) || default_username
 
 file_names = Dir["#{folder_path}/**/*.*"]
-ignored_file_types = ['.xccheckout',
-                      '.xcodeproj',
-                      '.xcworkspace',
-                      '.xcuserdatad',
-                      '.xcuserstate',
-                      '.rb']
+file_names.push("#{folder_path}/SwiftPackage.xcodeproj/project.pbxproj")
+file_names.push("#{folder_path}/SwiftPackage.xcodeproj/project.xcworkspace/contents.xcworkspacedata")
+file_names.push("#{folder_path}/SwiftPackage.xcodeproj/xcshareddata/xcschemes/SwiftPackage-iOS.xcscheme")
+file_names.push("#{folder_path}/SwiftPackage.xcodeproj/xcshareddata/xcschemes/SwiftPackage-Mac.xcscheme")
+file_names.push("#{folder_path}/SwiftPackage.xcodeproj/xcshareddata/xcschemes/SwiftPackage-iOS-Tests.xcscheme")
+file_names.push("#{folder_path}/SwiftPackage.xcodeproj/xcshareddata/xcschemes/SwiftPackage-Mac-Tests.xcscheme")
 
 file_names.each do |file_name|
+  ignored_file_types = ['.xccheckout',
+    '.xcodeproj',
+    '.xcworkspace',
+    '.xcuserdatad',
+    '.xcuserstate',
+    '.xcassets',
+    '.appiconset',
+    '.png',
+    '.lproj',
+    '.rb']
+
   if !ignored_file_types.include?(File.extname(file_name))
     text = File.read(file_name)
 
-    new_contents = text.gsub(/<PODNAME>/, pod_name)
+    new_contents = text.gsub(/<PACKAGENAME>/, package_name)
+    new_contents = new_contents.gsub(/SwiftPackage/, package_name)
+    new_contents = new_contents.gsub(/BundleDomain/, bundle_domain)
     new_contents = new_contents.gsub(/<AUTHOR_NAME>/, author_name)
     new_contents = new_contents.gsub(/<AUTHOR_EMAIL>/, author_email)
     new_contents = new_contents.gsub(/<USERNAME>/, username)
@@ -42,11 +57,23 @@ file_names.each do |file_name|
   end
 end
 
-File.rename("#{folder_path}/PODNAME.podspec", "#{folder_path}/#{pod_name}.podspec")
+File.rename("#{folder_path}/SwiftPackage.podspec", "#{folder_path}/#{package_name}.podspec")
+File.rename("#{folder_path}/SwiftPackage", "#{folder_path}/#{package_name}")
+File.rename("#{folder_path}/SwiftPackageTests", "#{folder_path}/#{package_name}Tests")
+File.rename("#{folder_path}/SwiftPackage.xcodeproj/xcshareddata/xcschemes/SwiftPackage-iOS.xcscheme",
+  "#{folder_path}/SwiftPackage.xcodeproj/xcshareddata/xcschemes/#{package_name}-iOS.xcscheme")
+File.rename("#{folder_path}/SwiftPackage.xcodeproj/xcshareddata/xcschemes/SwiftPackage-Mac.xcscheme",
+  "#{folder_path}/SwiftPackage.xcodeproj/xcshareddata/xcschemes/#{package_name}-Mac.xcscheme")
+File.rename("#{folder_path}/SwiftPackage.xcodeproj/xcshareddata/xcschemes/SwiftPackage-iOS-Tests.xcscheme",
+  "#{folder_path}/SwiftPackage.xcodeproj/xcshareddata/xcschemes/#{package_name}-iOS-Tests.xcscheme")
+File.rename("#{folder_path}/SwiftPackage.xcodeproj/xcshareddata/xcschemes/SwiftPackage-Mac-Tests.xcscheme",
+  "#{folder_path}/SwiftPackage.xcodeproj/xcshareddata/xcschemes/#{package_name}-Mac-Tests.xcscheme")
+File.rename("#{folder_path}/SwiftPackage.xcodeproj", "#{folder_path}/#{package_name}.xcodeproj")
 
 git_directory = "#{folder_path}/.git"
 FileUtils.rm_rf git_directory
 FileUtils.rm('init.rb')
 
 system("git init && git add . && git commit -am 'Initial commit'")
-system("git remote add origin https://github.com/#{username}/#{pod_name}.git")
+system("git remote add origin https://github.com/#{username}/#{package_name}.git")
+system("open \"#{folder_path}/#{package_name}.xcodeproj\"")
