42d5a346ca8481544a7214577970449571f1ea32
Khoa Pham <onmyway133@gmail.com>
2017-09-25 11:32:44 +0200

    Add tvOS and watchOS

---
+++ SwiftPackage-tvOS/SwiftPackage_tvOS.h
@@ -0,0 +1,19 @@
+//
+//  SwiftPackage_tvOS.h
+//  SwiftPackage-tvOS
+//
+//  Created by Khoa Pham on 25.09.2017.
+//  Copyright © 2017 <AUTHOR_NAME>. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftPackage_tvOS.
+FOUNDATION_EXPORT double SwiftPackage_tvOSVersionNumber;
+
+//! Project version string for SwiftPackage_tvOS.
+FOUNDATION_EXPORT const unsigned char SwiftPackage_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftPackage_tvOS/PublicHeader.h>
+
+
